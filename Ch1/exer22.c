01568977bb547e78c2dd05dfa47c6a24c7d36150
Abhinav Patil <apatil.cs@gmail.com>
2018-10-07 01:10:27 -0400

    Worked some more on 1.22

--- Ch1/exer22.c
+++ Ch1/exer22.c
@@ -1,12 +1,12 @@
 #include <stdio.h>
 #define MAXLINE 10
-#define MAXCOLUMNS 8
+#define COL_SIZE 8
 
-void println_up_to(char line[], int lim);
+void println_up_to(char line[], int start, int end);
 
-/* Program to "fold" lines above a certain length (80 columns) into two or more
- * lines. Breaks lines at the last non-blank character before the 80th column.
- * If there is no whitespace before the 80th column, breaks the line at the
+/* Program to "fold" lines above a certain length (MAXLINE) into two or more
+ * lines. Breaks lines at the last non-blank character before the MAXLINEth
+ * column. If there is no whitespace before that column, breaks the line at the
  * furthest left (i.e. earliest encountered) blank. If there are no blanks in
  * the entire line, prints the entire line as-is.
  */
@@ -17,42 +17,44 @@ main()
      * in the line.
      * numcols counts the number of columns (accounting for the behavior of
      * tabs).
-     * col counts (from 0 to MAXCOLUMNS) the number of columns since the last
+     * col counts (from 0 to COL_SIZE) the number of columns since the last
      * tabstop.
      * lastblank indicates the index of the last blank character in the array 
      * line.
      */
     int c, pos, numcols, col, lastblank;
     char line[MAXLINE];
-    numcols = col = 0;
-    pos = lastblank = -1;
+    pos = numcols = col = 0;
+    lastblank = -1;
 
     while ((c = getchar()) != EOF) {
-	if (numcols == MAXLINE - 1 || (c == '\t' && (numcols + MAXCOLUMNS - col) == (MAXLINE - 1))) { /* at the boundary */
+	if (numcols == MAXLINE - 1 || (c == '\t' && (numcols + COL_SIZE - col) == (MAXLINE - 1))) { /* at the boundary */
 	    if (c == '\n' || c == ' ' || c == '\t') { /* whitespace, so terminate/fold line */
-		println_up_to(line, MAXLINE - 1);
+		println_up_to(line, 0, MAXLINE - 1);
 		putchar('\n');
 
-		numcols = col = 0;
-		pos = lastblank = -1;
+		pos = numcols = col = 0;
+		lastblank = -1;
 	    }
 	    else { /* non whitespace characters, so fold intelligently */
 		if (lastblank < 0) { /* no blanks so far in this line */
-		    println_up_to(line, MAXLINE - 1);
+		    println_up_to(line, 0, MAXLINE - 1);
 		    putchar(c);
 		    ++pos, ++numcols;
 
-		    if (col == MAXCOLUMNS)
+		    if (col == COL_SIZE)
 			col = 0;
 		    else
 			++col;
 		}
 		else { /* there was a blank earlier, so print up to it */
-		    println_up_to(line, lastblank);
+                    println_up_to(line, 0, lastblank);
 		    putchar('\n');
+		    /* copy everything past the blank to the beginning */
+		    println_up_to(line, lastblank, pos);
 		    putchar(c);
-		    numcols = col = 0;
-		    pos = lastblank = -1;
+		    pos = numcols = col = 0;
+		    lastblank = -1;
 		}
 	    }
 	}
@@ -61,45 +63,45 @@ main()
 	else if (numcols >= MAXLINE) {
 	    if (c == '\n' || c == ' ' || c == '\t') {
 		putchar('\n');
-		numcols = col = 0;
-		pos = lastblank = -1;
+		pos = numcols = col = 0;
+		lastblank = -1;
 	    }
 	    else {
 		putchar(c);
 		++pos, ++numcols;
 		
-		if (col == MAXCOLUMNS)
+		if (col == COL_SIZE)
 		    col = 0;
 		else
 		    ++col;
 	    }
 	}
 	else { /* before the boundary */
-	    ++pos;
 	    if (c == '\n') {
-		println_up_to(line, pos);
+		println_up_to(line, 0, pos);
 		putchar('\n');
-		numcols = col = 0;
-		pos = lastblank = -1;
+		pos = numcols = col = 0;
+		lastblank = -1;
 	    }
 	    else {
-		line[pos] = c;
+                line[pos] = c;
 		if (c == ' ' || c == '\t') {
 		    lastblank = pos;
 		}
 
 		if (c == '\t') {
-		    numcols += MAXCOLUMNS - col;
+		    numcols += COL_SIZE - col;
 		    col = 0;
 		}
 		else {
-		    if (col == MAXCOLUMNS)
+		    if (col == COL_SIZE)
 			col = 0;
 		    else
 			++col;
 
 		    ++numcols;
 		}
+                pos++;
 	    }
 	}
     }
@@ -107,9 +109,9 @@ main()
 return 0;
 }
 
-void println_up_to(char line[], int lim) {
+void println_up_to(char line[], int start, int end) {
     int i;
 
-    for (i = 0; i < lim; ++i)
+    for (i = start; i < end; ++i)
 	putchar(line[i]);
 }
